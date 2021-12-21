e20c6c3bbb8e71e455068713b4003ab5b09c1c8d
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-12-21 23:05:50 +0700

    change function to get stats from api

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,13 +3,13 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
-import { useGetExchangesQuery } from '../services/cryptoApi';
+import { useGetStatsQuery } from '../services/cryptoApi';
 import { Cryptocurrencies, News } from '../components';
 
 const { Title, Text } = Typography;
 
 const Homepage = () => {
-    const { data, isFetching } = useGetExchangesQuery();
+    const { data, isFetching } = useGetStatsQuery();
     const globalStats = data?.data;
 
     if (isFetching) return 'Loading...';
